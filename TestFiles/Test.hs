162fcb5ee9f35e0790343e18c71120fb60be7428
Rajib Chandra Das <aporba.das@gmail.com>
2018-08-10 00:28:38 +0600

    Add Solutions for Chapter 04 DefiningFunctions

--- TestFiles/Test.hs
+++ TestFiles/Test.hs
@@ -4,3 +4,11 @@ factorial x = product [1..x]
 average xs = sum xs `div` length xs
 
 
+myAdd a b = a + b
+
+myAnd a b = if a == False then False
+		else if b == False then False
+		else True
+
+		
+myMult = \x -> (\y -> (\z -> x * y * z))
